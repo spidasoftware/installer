171745dacdb38b418561fe82aaf7568169cb8d9b
Jeremy Wentworth <jeremy.wentworth@spidasoftware.com>
2018-11-13 14:01:02 -0500

    config and scripts for virus scanning

---
+++ config/clamav/inotify-scan.sh
@@ -0,0 +1,13 @@
+LAST_HASH=""
+inotifywait -m -r -e close_write --format '%w%f' --exclude \.git /apps/spidamin/files/ | while read FILE_PATH 
+do 
+    if [ -f "$FILE_PATH" ]; then #check if file (not a dir)
+        FILE_HASH=$(md5sum "$FILE_PATH")
+        if [ "$FILE_HASH" != "$LAST_HASH" ];then #prevent infinite loop
+
+            LAST_HASH="$FILE_HASH"
+
+            clamdscan --no-summary "$FILE_PATH"
+        fi
+    fi
+done
